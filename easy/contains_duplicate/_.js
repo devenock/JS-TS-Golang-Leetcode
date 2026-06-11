1ef4730f43214bf22d8622dedc1825584984652a
devenock <enockomondi305@gmail.com>
2026-06-11 23:03:54 +0300

    contains duplicate JS solution

---
+++ easy/contains_duplicate/_.js
@@ -0,0 +1,32 @@
+// PROBLEM
+// Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+// Example 1:
+// Input: nums = [1, 2, 3, 3]
+// Output: true
+// Example 2:
+// Input: nums = [1, 2, 3, 4]
+// Output: false
+
+//SOLUTION
+function hasDuplicate(nums) {
+  // check if the array is empty
+  if (nums.length == 0) {
+    return false;
+  }
+
+  // define a frequency object
+  let numsFrequency = {};
+
+  for (let val of nums) {
+    numsFrequency[val] = (numsFrequency[val] || 0) + 1;
+  }
+
+  for (let key in numsFrequency) {
+    if (numsFrequency[key] > 1) {
+      return true;
+    }
+  }
+  return false;
+}
+
+console.log(hasDuplicate([1, 2, 3, 3]));
